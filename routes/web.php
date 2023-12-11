785fdf2d8bc0900e7cb05762814abe5a25e6cdea
phphuc0608 <phuc31082002@gmail.com>
2023-12-11 17:04:38 +0700

    finish champ skin detail UI and fix bug in controller

--- routes/web.php
+++ routes/web.php
@@ -88,7 +88,7 @@ Route::get('xoa_item_process/{ma_vat_pham}', [\App\Http\Controllers\San_pham_con
 Route::get('home_ds_tuong', [App\Http\Controllers\Danh_sach_tuong_controller::class, 'view_home_ds_tuong'])->name('home_ds_tuong');
 //trang_phuc
 Route::get('home_mua_trang_phuc',[App\Http\Controllers\San_pham_controller::class, 'view_home_mua_trang_phuc'])->name('home_mua_trang_phuc');
-Route::get('home_chi_tiet_trang_phuc}',[App\Http\Controllers\San_pham_controller::class, 'view_home_chi_tiet_trang_phuc'])->name('home_chi_tiet_trang_phuc');
+Route::get('home_chi_tiet_trang_phuc',[App\Http\Controllers\San_pham_controller::class, 'view_home_chi_tiet_trang_phuc'])->name('home_chi_tiet_trang_phuc');
 //bau_vat
 Route::get('home_mua_bau_vat',[App\Http\Controllers\San_pham_controller::class, 'view_home_mua_bau_vat'])->name('home_mua_bau_vat');
 //vat_pham
